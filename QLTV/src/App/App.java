8e25afbb3a802708feacd6da2e880167129beb86
dangvantu99CM <vantund2017@gmail.com>
2021-01-09 21:05:40 +0700

    Da hoan thanh

--- QLTV/src/App/App.java
+++ QLTV/src/App/App.java
@@ -5,6 +5,7 @@
  */
 package App;
 
+import View.MainManager.Manager;
 import View.Quan_ly_tai_khoan.FormLogin;
 
 /**
@@ -12,7 +13,25 @@ import View.Quan_ly_tai_khoan.FormLogin;
  * @author tudv
  */
 public class App {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Manager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Manager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Manager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Manager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
         FormLogin formLogin = new FormLogin();
     }
 }
